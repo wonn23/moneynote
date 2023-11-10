c59a50d42f12dbb693ee98a7a332a4c547413c10
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-11-11 00:52:26 +0900

    feat: 카테고리 엔터티 작성

---
+++ src/budget/entities/category.entity.ts
@@ -0,0 +1,9 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+@Entity('categories')
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+}
