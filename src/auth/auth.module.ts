95f2dbab5e8681b4f8bdadcde5d187cd656584b8
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-11-10 09:09:53 +0900

    feat: 회원 로그인 구현

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './service/auth.service'
+import { AuthController } from './controller/auth.controller'
 
 @Module({
   controllers: [AuthController],
