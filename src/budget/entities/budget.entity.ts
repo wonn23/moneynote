0b5e36786f5a5bcb9b263b3645c44eda8c4a62a4
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-11-11 00:52:02 +0900

    feat: 예산 엔터티 작성

---
+++ src/budget/entities/budget.entity.ts
@@ -0,0 +1,36 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm'
+import { Category } from './category.entity'
+@Entity('budget')
+export class Budget extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'varchar', length: 7 })
+  yearMonth: string
+
+  @Column()
+  amount: number
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @DeleteDateColumn()
+  deletedAt: Date
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'categoryId' })
+  category: Category
+}
