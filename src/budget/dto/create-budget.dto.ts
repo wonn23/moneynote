a1c396147430cc4a28bb09974d93e3a2b6da1b33
heochangwon <wonn22@gmail.com>
2024-01-29 03:35:00 +0900

    fix: 예산DTO enum 설정

--- src/budget/dto/create-budget.dto.ts
+++ src/budget/dto/create-budget.dto.ts
@@ -1,20 +1,24 @@
-import { IsEnum, IsInt, IsString, Matches, Min } from 'class-validator'
+import {
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsString,
+  Matches,
+  Min,
+} from 'class-validator'
 import { categoryEnum } from '../types/budget.enum'
 
-const YYYY_MM_Format = /^\d{4}-(1[0-2]|0[1-9])$/
-
 export class CreateBudgetDto {
-  @Matches(YYYY_MM_Format, {
-    message:
-      'yearMonth는 "YYYY-MM" 형식이어야 하며 MM은 01부터 12까지 가능합니다.',
-  })
-  @IsString()
-  yearMonth: string
+  @IsInt()
+  year: number
+
+  @IsInt()
+  month: number
 
   @IsInt()
   @Min(0, { message: '금액은 0원 이상이어야 합니다.' })
   amount: number
 
   @IsEnum(categoryEnum)
-  category: string
+  category: categoryEnum
 }
