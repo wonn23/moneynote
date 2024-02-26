5e09a117441e33349f4195f83d041b5d9e93144b
heochangwon <wonn22@gmail.com>
2024-02-26 23:24:57 +0900

    fix: 유저 레포지토리 create 함수 삭제

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -5,11 +5,6 @@ import { User } from '../entities/user.entity'
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
-  // 유저 생성
-  async createUser(user: CreateUserDto): Promise<void> {
-    await this.save(user)
-  }
-
   // username으로 유저 찾기
   async findByUsername(username: string): Promise<User> {
     const user = await this.findOne({ where: { username } })
