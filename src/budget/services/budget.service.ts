5cca4b0a7ee9f247740a34eeb4181675af56b4cc
heochangwon <wonn22@gmail.com>
2024-02-29 21:14:26 +0900

    feat: 예산 서비스 테스트 코드 작성 완료

--- src/budget/services/budget.service.ts
+++ src/budget/services/budget.service.ts
@@ -4,33 +4,22 @@ import {
   NotFoundException,
 } from '@nestjs/common'
 import { UpdateBudgetDto } from '../dto/update-budget.dto'
-import { BudgetRepository } from '../repositories/budget.repository'
-import { CategoryRepository } from '../repositories/category.repository'
 import { CreateBudgetDto } from '../dto/create-budget.dto'
-import { EntityNotFoundError } from 'typeorm'
+import { DataSource, Repository } from 'typeorm'
 import { Budget } from '../entities/budget.entity'
-import { BudgetStatsService } from './budgetStats.service'
-import { BudgetRecommendationDto } from '../dto/budget-recommendation.dto'
-import { privateDecrypt } from 'crypto'
 import { User } from 'src/user/entities/user.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Category } from '../entities/category.entity'
 
 @Injectable()
 export class BudgetService {
-  private minimumRatio = 10
   constructor(
-    private readonly budgetRepository: BudgetRepository,
-    private readonly categoryRepository: CategoryRepository,
-    private readonly budgetStatsService: BudgetStatsService,
+    @InjectRepository(Budget)
+    private budgetRepository: Repository<Budget>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+    private dataSource: DataSource,
   ) {}
-
-  // 예산 총합 구하기
-  getTotalAmount(arr: number[]): number {
-    let total: number = 0
-    arr.forEach((price) => {
-      total += price
-    })
-    return total
-  }
 
   async createBudget(
     createBudgetDto: CreateBudgetDto,
@@ -60,128 +49,108 @@ export class BudgetService {
     }
   }
 
-  async designBudget(totalAmount: number): Promise<BudgetRecommendationDto> {
-    const averageRatios = await this.budgetStatsService.getAverageBudgetRatios()
-    let budgetRecommendations = {
-      food: 0,
-      transportation: 0,
-      curtureLife: 0,
-      housingCommunication: 0,
-      dailyNeccessities: 0,
-      other: 0,
-    }
-
-    for (const [category, ratio] of Object.entries(averageRatios)) {
-      // ratio가 숫자인지 확인하기
-      const numericRatio = typeof ratio === 'number' ? ratio : parseFloat(ratio)
-
-      // ratio 유효성 검사
-      if (isNaN(numericRatio)) {
-        throw new Error(`${category}에 대한 비율이 유효하지 않습니다.`)
-      }
-      const amountForCategory = totalAmount * numericRatio
-
-      if (numericRatio < 0.1) {
-        budgetRecommendations.other += amountForCategory
-      } else {
-        if (category in budgetRecommendations) {
-          budgetRecommendations[category] += amountForCategory
-        } else {
-          // 미리 정의된 카테고리가 아닐 경우
-          budgetRecommendations.other += amountForCategory
-        }
-      }
-    }
-
-    return new BudgetRecommendationDto(budgetRecommendations)
-  }
-
-  // 총액 비례 비율에 맞는 금액 계산
-  getPercentageTotalAmount(ratio: object[], totalPrice: number): object[] {
-    /**
-     * 카테고리별 예산 비율을 바탕으로 전체 예산 대비 각 카테고리의 예산을 계산하고, 10% 이하의 카테고리들을 '그외'로
-     * 분류하여 그 합계를 계산합니다.
-     * 'ratio' 객체 배열과 'totalPrice' 총 예산을 사용하여 각 카테고리의 비율에 따른 예산을 계산하고 객체 배열로 반환합니다.
-     */
-    ratio = ratio.map((obj) => ({
-      ...obj,
-      ratio: Number(obj['ratio']),
-    }))
-    const percentages = ratio.filter(
-      (percentageCategory) => percentageCategory['ratio'] >= this.minimumRatio,
-    )
-    const etc = ratio
-      .filter(
-        (percentageCategory) => percentageCategory['ratio'] < this.minimumRatio,
-      )
-      .reduce((sum, current) => {
-        return sum + current['ratio']
-      }, 0)
-
-    percentages.push({ category_name: '그외', ratio: etc })
-
-    const percentageAmount = percentages.map((percentageCategory) => ({
-      ...percentageCategory,
-      amount:
-        Math.floor((totalPrice * (percentageCategory['ratio'] / 100)) / 1000) *
-        1000,
-    }))
-
-    return percentageAmount
-  }
-
-  async getUserAverageRatio(totalPrice: number): Promise<object[]> {
-    /**
-     * 서브 쿼리 생성
-     * budgetRepository를 사용해서 Budget 엔티티에 대한 쿼리 빌더를 생성합니다.
-     * 이 쿼리는 각 category_id별로 amount의 합계('total_amount')와 모든 예산의 합계('total')를
-     * 계산하는 서브 쿼리를 생성합니다.
-     * groupBy('category_id')를 통해 카테고리별로 그룹화하고 결과적으로 각 카테고리에서 지툴된 총액과 전체 지출액을 계산합니다.
-     *
-     * 메인 쿼리 실행
-     * categoryRepository를 사용하여 Category 엔티티에 대한 쿼리를 구성합니다.
-     * 이 쿼리는 카테고리의 이름과 각 카테고리별 지출 총액이 전체 지출액에서 차지하는 비율(ratio)을 선택하여 반환합니다.
-     * 비율은 ROUND 함수를 사용하여 소수점 둘째 자리까지 반올림합니다.
-     * 서브 쿼리의 결과(subQuery)는 innerJoin을 통해 메인 쿼리에 조인되며, 이를 통해 각 카테고리의 이름과 해당 카테고리의 지출 비율이 계산됩니다.
-     *
-     * 결과
-     * getRawMany()를 호출하여 최종결과를 얻습니다. 이 결과는 카테고리의 이름과 해당 카테고리의 지출 비율을 포함합니다.
-     */
+  async designBudget(totalAmount: number, year: number, month: number) {
     try {
-      const subQuery = this.budgetRepository
-        .createQueryBuilder()
-        .subQuery()
-        .select([
-          'category_id',
-          'SUM(amount) as total_amount',
-          '(select sum(total) from budgets) as total',
-        ])
-        .from(Budget, 'budget')
-        .groupBy('category_id')
-        .getQuery()
+      const ratios = await this.getAverageCategoryRatios(year, month)
+      if (!ratios || ratios.length === 0) {
+        throw new NotFoundException(
+          '해당 연도와 월의 예산 데이터가 존재하지 않습니다.',
+        )
+      }
 
-      const result = this.categoryRepository
-        .createQueryBuilder('category')
-        .select([
-          'category.name',
-          'ROUND(sub.total_amount * 100.0:: decimal/sub.total:: decimal, 2) as ratio',
-        ])
-        .innerJoin(subQuery, 'sub', 'sub.category_id = category.id')
-        .orderBy('ratio', 'DESC')
+      const totalCategoryRatio =
+        ratios.find((ratio) => ratio.name === '전체')?.ratio || 1
 
-      const ratio = await result.getRawMany()
-      return this.getPercentageTotalAmount(ratio, totalPrice)
+      const budgetDesigns = ratios
+        .filter((ratio) => ratio.name !== '전체')
+        .map((ratio) => {
+          const calculateAmount =
+            (totalAmount * ratio.ratio) / totalCategoryRatio
+          return {
+            category: ratio.name,
+            budget: Math.round(calculateAmount),
+          }
+        })
+
+      const otherCategories = budgetDesigns.filter(
+        (budget) => budget.budget <= totalAmount * 0.1,
+      )
+
+      const otherTotalBudget = otherCategories.reduce(
+        (total, curr) => total + curr.budget,
+        0,
+      )
+
+      const filteredBudgets = budgetDesigns.filter(
+        (budget) => budget.budget > totalAmount * 0.1,
+      )
+      if (otherTotalBudget > 0) {
+        filteredBudgets.push({ category: '기타', budget: otherTotalBudget })
+      }
+
+      return filteredBudgets
     } catch (error) {
       throw new InternalServerErrorException(
-        '추천 예산을 불러오는 도중 에러가 발생했습니다.',
+        '예산 설계 중 에러가 발생했습니다.',
       )
     }
   }
 
-  async findBudgetByYear(year: number): Promise<Budget[]> {
-    const budgets = await this.budgetRepository.find({
-      where: { year },
-    })
+  async getAverageCategoryRatios(year: number, month: number) {
+    // 해당 year과 month에 해당하는 전체 유저의 예산 평균 비율을 계산한다.
+    try {
+      const ratios = await this.budgetRepository
+        .createQueryBuilder('budget')
+        .select('category.name', 'name')
+        .addSelect('AVG(budget.amount)', 'averageAmount')
+        .innerJoin(Category, 'category', 'budget.category_id = category.id')
+        .where('budget.year = :year AND budget.month = :month', { year, month })
+        .groupBy('category.name')
+        .getRawMany()
+
+      if (ratios.length === 0) {
+        return [
+          { name: '식사', ratio: 0.35 },
+          { name: '교통', ratio: 0.13 },
+          { name: '문화생활', ratio: 0.15 },
+          { name: '생활용품', ratio: 0.12 },
+          { name: '주거/통신', ratio: 0.25 },
+        ]
+      } else {
+        const totalBudget = ratios.find((ratio) => ratio.name === '전체')
+          ?.averageAmount
+        if (totalBudget) {
+          return ratios
+            .map((ratio) => {
+              if (ratio.name !== '전체') {
+                return {
+                  name: ratio.name,
+                  ratio: (
+                    parseFloat(ratio.averageAmount) / parseFloat(totalBudget)
+                  ).toFixed(3),
+                }
+              }
+            })
+            .filter(Boolean) // '전체' 카테고리 제외
+        }
+      }
+
+      return ratios
+    } catch (error) {
+      throw new InternalServerErrorException(
+        '예산 비율 계산 중 에러가 발생했습니다.',
+      )
+    }
+  }
+
+  async findBudgetByYear(year: number, user: User): Promise<Budget[]> {
+    const budgets = await this.budgetRepository
+      .createQueryBuilder('budget')
+      .where('budget.year = :year AND budget.user_id = :user', {
+        year,
+        user,
+      })
+      .getRawMany()
 
     if (!budgets.length) {
       throw new NotFoundException(
@@ -195,12 +164,17 @@ export class BudgetService {
   async findBudgetByYearAndMonth(
     year: number,
     month: number,
+    user: User,
   ): Promise<Budget[]> {
-    const budgets = await this.budgetRepository.find({
-      where: { year, month },
-    })
+    const budgets = await this.budgetRepository
+      .createQueryBuilder('budget')
+      .where(
+        'budget.user_id = :user AND budget.year =:year AND budget.month = :month',
+        { year, month, user },
+      )
+      .getRawMany()
 
-    if (budgets.length) {
+    if (!budgets.length) {
       throw new NotFoundException(
         `해당 연도(${year})와 월(${month})의 예산 데이터를 찾을 수 없습니다.`,
       )
@@ -211,24 +185,43 @@ export class BudgetService {
   async updateBudget(
     id: number,
     updateBudgetDto: UpdateBudgetDto,
+    user: User,
   ): Promise<Budget> {
+    const queryRunner = this.dataSource.createQueryRunner()
+    await queryRunner.connect()
+    await queryRunner.startTransaction()
+
     try {
-      const budget = await this.budgetRepository.findOneOrFail({
-        where: { id },
+      const category = await queryRunner.manager.findOne(Category, {
+        where: { name: updateBudgetDto.category },
       })
 
-      Object.assign(budget, updateBudgetDto)
-      await this.budgetRepository.save(budget)
+      if (!category) {
+        throw new NotFoundException('카테고리를 찾을 수 없습니다.')
+      }
 
-      return budget
-    } catch (error) {
-      if (error instanceof EntityNotFoundError) {
+      const budget = await queryRunner.manager.findOne(Budget, {
+        where: { id, user: { id: user.id } },
+        relations: ['category', 'user'],
+      })
+
+      if (!budget) {
         throw new NotFoundException(`해당 ID(${id})의 예산을 찾을 수 없습니다.`)
       }
 
-      throw new InternalServerErrorException(
-        '예산을 수정하는 데 문제가 발생했습니다.',
-      )
+      budget.amount = updateBudgetDto.amount
+      budget.category = category // 업데이트된 카테고리로 설정
+
+      await queryRunner.manager.save(budget)
+
+      await queryRunner.commitTransaction()
+
+      return budget
+    } catch (error) {
+      await queryRunner.rollbackTransaction()
+      throw new InternalServerErrorException('예산을 수정하는데 실패했습니다.')
+    } finally {
+      await queryRunner.release()
     }
   }
 
--- src/budget/test/budget.service.spec.ts
+++ src/budget/test/budget.service.spec.ts
@@ -1,18 +1,349 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { BudgetService } from '../services/budget.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Budget } from '../entities/budget.entity'
+import { Category } from '../entities/category.entity'
+import { DataSource, Repository } from 'typeorm'
+import { User } from 'src/user/entities/user.entity'
+import { categoryEnum } from '../types/budget.enum'
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common'
+import { UpdateBudgetDto } from '../dto/update-budget.dto'
+
+const mockBudgetRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  findOne: jest.fn(),
+  find: jest.fn(),
+  delete: jest.fn(),
+  createQueryBuilder: jest.fn(),
+}
+
+const mockCategoryRepository = {
+  findOne: jest.fn(),
+}
+
+const mockDataSource = {
+  createQueryRunner: jest.fn().mockReturnValue({
+    connect: jest.fn(),
+    startTransaction: jest.fn(),
+    manager: mockBudgetRepository,
+    commitTransaction: jest.fn(),
+    rollbackTransaction: jest.fn(),
+    release: jest.fn(),
+  }),
+}
 
 describe('BudgetService', () => {
   let service: BudgetService
+  let budgetRepository: Repository<Budget>
+  let categoryRepository: Repository<Category>
+  let dataSource: DataSource
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BudgetService],
+      providers: [
+        BudgetService,
+        {
+          provide: getRepositoryToken(Budget),
+          useValue: mockBudgetRepository,
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockCategoryRepository,
+        },
+        { provide: DataSource, useValue: mockDataSource },
+      ],
     }).compile()
 
     service = module.get<BudgetService>(BudgetService)
+    budgetRepository = module.get<Repository<Budget>>(
+      getRepositoryToken(Budget),
+    )
+    categoryRepository = module.get<Repository<Category>>(
+      getRepositoryToken(Category),
+    )
+    dataSource = module.get(DataSource)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+    expect(budgetRepository).toBeDefined()
+    expect(categoryRepository).toBeDefined()
+    expect(dataSource).toBeDefined()
+  })
+
+  describe('createBudget', () => {
+    it('예산 생성에 성공했습니다.', async () => {
+      const createBudgetDto = {
+        year: 2024,
+        month: 1,
+        amount: 1000000,
+        category: categoryEnum.food,
+      }
+      const user = { id: 'user-id', username: 'testUser' } as User
+
+      mockCategoryRepository.findOne.mockResolvedValueOnce(new Category())
+      mockBudgetRepository.save.mockImplementationOnce((budget) =>
+        Promise.resolve({ ...budget }),
+      )
+
+      await expect(
+        service.createBudget(createBudgetDto, user),
+      ).resolves.not.toThrow()
+    })
+  })
+
+  describe('designBudget', () => {
+    it('설정한 전체 예산과 유저들의 평균 비율을 게산하여 예산을 설계합니다.', async () => {
+      const totalAmount = 2000000
+      const year = 2024
+      const month = 1
+      jest.spyOn(service, 'getAverageCategoryRatios').mockResolvedValue([
+        { name: '식사', ratio: '0.500' },
+        { name: '교통', ratio: '0.075' },
+        { name: '문화생활', ratio: '0.175' },
+        { name: '생활용품', ratio: '0.100' },
+        { name: '주거/통신', ratio: '0.150' },
+      ])
+
+      const designBudget = await service.designBudget(totalAmount, year, month)
+      expect(designBudget).toEqual([
+        { category: '식사', budget: 1000000 },
+        { category: '문화생활', budget: 350000 },
+        { category: '주거/통신', budget: 300000 },
+        { category: '기타', budget: 350000 },
+      ])
+    })
+
+    it('예산 설계 도출 중 InternalServerErrorException에러 발생합니다.', async () => {
+      const totalAmount = 10000000
+      const year = 2024
+      const month = 1
+      jest.spyOn(service, 'getAverageCategoryRatios').mockResolvedValue([])
+
+      await expect(
+        service.designBudget(totalAmount, year, month),
+      ).rejects.toThrow(InternalServerErrorException)
+    })
+  })
+
+  describe('getAverageCategoryRatios', () => {
+    it('데이터를 찾았을 때 계산된 비율을 return 합니다.', async () => {
+      const year = 2024
+      const month = 1
+      mockBudgetRepository.createQueryBuilder.mockReturnValue({
+        select: jest.fn().mockReturnThis(),
+        addSelect: jest.fn().mockReturnThis(),
+        innerJoin: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        groupBy: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockResolvedValue([
+          { name: '전체', averageAmount: '2000000' },
+          { name: '식사', averageAmount: '1000000' },
+          { name: '교통', averageAmount: '150000' },
+          { name: '문화생활', averageAmount: '350000' },
+          { name: '생활용품', averageAmount: '200000' },
+          { name: '주거/통신', averageAmount: '300000' },
+        ]),
+      })
+
+      const ratios = await service.getAverageCategoryRatios(year, month)
+      expect(ratios).toEqual([
+        { name: '식사', ratio: '0.500' },
+        { name: '교통', ratio: '0.075' },
+        { name: '문화생활', ratio: '0.175' },
+        { name: '생활용품', ratio: '0.100' },
+        { name: '주거/통신', ratio: '0.150' },
+      ])
+    })
+
+    it('데이터를 찾지 못해 기본 비율을 return 합니다.', async () => {
+      const year = 2024
+      const month = 1
+      mockBudgetRepository.createQueryBuilder.mockReturnValue({
+        select: jest.fn().mockReturnThis(),
+        addSelect: jest.fn().mockReturnThis(),
+        innerJoin: jest.fn().mockReturnThis(),
+        where: jest.fn().mockReturnThis(),
+        groupBy: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockResolvedValue([
+          { name: '식사', ratio: '0.35' },
+          { name: '교통', ratio: '0.13' },
+          { name: '문화생활', ratio: '0.15' },
+          { name: '생활용품', ratio: '0.12' },
+          { name: '주거/통신', ratio: '0.25' },
+        ]),
+      })
+
+      const ratios = await service.getAverageCategoryRatios(year, month)
+      expect(ratios).toEqual([
+        { name: '식사', ratio: '0.35' },
+        { name: '교통', ratio: '0.13' },
+        { name: '문화생활', ratio: '0.15' },
+        { name: '생활용품', ratio: '0.12' },
+        { name: '주거/통신', ratio: '0.25' },
+      ])
+    })
+  })
+
+  describe('findBudgetByYear', () => {
+    it('해당 연도의 예산 데이터를 성공적으로 찾습니다.', async () => {
+      const year = 2024
+      const category = { id: 1 }
+      const user = { id: 'user-id' } as User
+      mockBudgetRepository
+        .createQueryBuilder()
+        .where()
+        .getRawMany.mockResolvedValue([
+          {
+            id: 1,
+            year: 2024,
+            month: 1,
+            amount: 1000000,
+            category_id: category.id,
+            user_id: user.id,
+          },
+          {
+            id: 2,
+            year: 2024,
+            month: 2,
+            amount: 2000000,
+            category_id: category.id,
+            user_id: user.id,
+          },
+        ])
+
+      const result = await service.findBudgetByYear(year, user)
+      expect(result).toHaveLength(2)
+      expect(result[0].year).toEqual(year)
+      expect(result[1].year).toEqual(year)
+    })
+
+    it('해당 연도의 예산 데이터가 없을 경우 NotFoundException을 발생시킵니다.', async () => {
+      const year = 2024
+      const user = { id: 'user-id' } as User
+
+      mockBudgetRepository
+        .createQueryBuilder()
+        .where()
+        .getRawMany.mockResolvedValue([])
+
+      await expect(service.findBudgetByYear(year, user)).rejects.toThrow(
+        NotFoundException,
+      )
+    })
+  })
+
+  describe('findBudgetByYearAndMonth', () => {
+    it('해당 연도와 월의 예산 데이터를 성공적으로 찾습니다.', async () => {
+      const year = 2024
+      const month = 1
+      const category = { id: 1 }
+      const user = { id: 'user-id' } as User
+
+      mockBudgetRepository
+        .createQueryBuilder()
+        .where()
+        .getRawMany.mockResolvedValue([
+          {
+            id: 1,
+            year: 2024,
+            month: 1,
+            amount: 1000000,
+            category_id: category.id,
+            user_id: user.id,
+          },
+          {
+            id: 2,
+            year: 2024,
+            month: 1,
+            amount: 3000000,
+            category_id: category.id,
+            user_id: user.id,
+          },
+        ])
+
+      const result = await service.findBudgetByYearAndMonth(year, month, user)
+      expect(result).toHaveLength(2)
+      expect(result[0].month).toEqual(month)
+      expect(result[1].month).toEqual(month)
+    })
+
+    it('해당 연도의 월의 예산 데이터가 없을 경우 NotFoundException을 발생시킵니다.', async () => {
+      const year = 2024
+      const month = 1
+      const user = { id: 'user-id' } as User
+
+      mockBudgetRepository
+        .createQueryBuilder()
+        .where()
+        .getRawMany.mockResolvedValue([])
+
+      await expect(
+        service.findBudgetByYearAndMonth(year, month, user),
+      ).rejects.toThrow(NotFoundException)
+    })
+  })
+
+  describe('updateBudget', () => {
+    it('예산을 성공적으로 수정합니다.', async () => {
+      const id = 1
+      const updatedBudgetDto: UpdateBudgetDto = {
+        amount: 200000,
+        category: categoryEnum.food,
+      }
+      const user = { id: 'user-id' } as User
+
+      mockDataSource
+        .createQueryRunner()
+        .manager.findOne.mockResolvedValueOnce(new Category())
+        .mockResolvedValueOnce(new Budget())
+
+      mockDataSource.createQueryRunner().manager.save.mockResolvedValue({
+        ...updatedBudgetDto,
+        id,
+        user,
+      })
+
+      const result = await service.updateBudget(id, updatedBudgetDto, user)
+      expect(result.amount).toEqual(updatedBudgetDto.amount)
+
+      it('카테고리를 찾을 수 없을 경우 NotFoundException을 발생시킵니다.', async () => {
+        const id = 1
+        const updateBudgetDto = {
+          amount: 200000,
+          category: '없는 카테고리' as any,
+        }
+        const user = { id: 'user-id' } as User
+
+        mockDataSource
+          .createQueryRunner()
+          .manager.findOne.mockResolvedValueOnce(null)
+
+        await expect(
+          service.updateBudget(id, updateBudgetDto, user),
+        ).rejects.toThrow(NotFoundException)
+      })
+    })
+  })
+
+  describe('deleteBudget', () => {
+    it('예산을 성공적으로 삭제합니다.', async () => {
+      const id = 1
+
+      mockBudgetRepository.delete.mockResolvedValue({ affected: 1 })
+
+      await expect(service.deleteBudget(id)).resolves.not.toThrow()
+      expect(mockBudgetRepository.delete).toHaveBeenCalledWith(id)
+    })
+
+    it('해당 id의 예산을 찾을 수 없을 경우 NotFOundException을 발생시킵니다.', async () => {
+      const id = 99
+
+      mockBudgetRepository.delete.mockResolvedValue({ affected: 0 })
+
+      await expect(service.deleteBudget(id)).rejects.toThrow(NotFoundException)
+      expect(mockBudgetRepository.delete).toHaveBeenCalledWith(id)
+    })
   })
 })
