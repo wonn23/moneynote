e078f01efe083277e569bf144796692614219801
heochangwon <wonn22@gmail.com>
2024-04-26 17:42:09 +0900

    fix: Update buildspec

--- test/utils.ts
+++ test/utils.ts
@@ -31,9 +31,7 @@ export async function closeNestApplication(
   try {
     const dataSource: DataSource = app.get(DataSource)
     if (dataSource && dataSource.isInitialized) {
-      console.log('데이터베이스 종료 직전!!')
       await dataSource.destroy()
-      console.log('데이터베이스 종료 완료')
     } else {
       console.log('데이터베이스 이미 종료됨 혹은 초기화되지 않음')
     }
@@ -41,11 +39,9 @@ export async function closeNestApplication(
     const redisService = app.get(ICACHE_SERVICE)
     const redisClient = redisService.cacheManager.store.getClient()
     await redisClient.quit()
-    console.log('Redis 연결 종료 완료')
   } catch (error) {
     console.error('Failed to close data source or Redis', error)
   }
 
   await app.close()
-  console.log('애플리케이션 종료 완료')
 }
